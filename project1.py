d0a4f0c9ae10fdc613586a79ca637f68d90a5b63
Emerson McKay <emckayy@umich.edu>
2025-10-06 20:33:26 -0400

    Read in the csv file I am using

---
+++ project1.py
@@ -0,0 +1,21 @@
+# Emerson McKay
+# umid: 68460607
+# collaborators: 
+
+
+import csv
+
+def read_csv_file(filename):
+    fhand = open(filename)
+    headers = fhand.readline().strip().split(",")
+    data = []
+    for line in fhand:
+        values = line.strip().split(",")
+        row_dict = dict(zip(headers, values))
+        data.append(row_dict)
+    fhand.close()
+    return data
+
+file_path = "C:/Users/emerm/OneDrive/Desktop/SI 201/fall25-project1-emersonmckay/crop_yield.csv"
+data = read_csv_file(file_path)
+print(data[:5])
