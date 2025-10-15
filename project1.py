9de53bca9f00179da0cdbd21a4164b4fdf309d1c
Emerson McKay <emckayy@umich.edu>
2025-10-15 18:51:24 -0400

    Created the test case for my first function

--- project1.py
+++ project1.py
@@ -5,6 +5,7 @@
 
 
 import csv
+import unittest
 
 def read_csv_file(filename):
     fhand = open(filename)
@@ -13,10 +14,33 @@ def read_csv_file(filename):
     for line in fhand:
         values = line.strip().split(",")
         row_dict = dict(zip(headers, values))
+
+        if "Yield_tons_per_hectare" in row_dict:
+            try:
+                row_dict["Yield_tons_per_hectare"] = float(row_dict["Yield_tons_per_hectare"])
+            except:
+                row_dict["Yield_tons_per_hectare"] = None
         data.append(row_dict)
-    fhand.close()
+
     return data
 
 file_path = "C:/Users/emerm/OneDrive/Desktop/SI 201/fall25-project1-emersonmckay/crop_yield.csv"
 data = read_csv_file(file_path)
-print(data[:5])
+
+def calculate_avg_yield_by_crop(data):
+    print("placeholder")
+
+
+# ************ TEST CASES ************
+class TestingCalculation(unittest.Testcase):
+    def test_calculate_avg_yield_by_crop(self):
+        data = [
+            {"Crop": "Wheat", "Yield_tons_per_hectare": 5.0},
+            {"Crop": "Wheat", "Yield_tons_per_hectare": 9.5},
+            {"Crop": "Ricet", "Yield_tons_per_hectare": 4.0},
+        ]
+
+        result = calculate_avg_yield_by_crop(data)
+        self.assertEqual(result["Wheat"], 7.25)
+        self.assertEqual(result["Rice"], 4.0)
+    
